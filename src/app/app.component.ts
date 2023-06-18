19c6ff37857db3f64630c619a6e2b0f8d45ac8c8
Jayesh <jayeshmishra121c@gmail.com>
2023-06-18 20:23:32 +0530

    Create Template Form with Validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  form = {
+    fName: '',
+    lastName: '',
+    pan: '',
+    mobile: '',
+    age: '',
+  };
+  registerFn() {
+    console.log(this.form);
+  }
+  integerRegex = '^[1-9]\\d*$';
   title = 'template-driven-form';
 }
